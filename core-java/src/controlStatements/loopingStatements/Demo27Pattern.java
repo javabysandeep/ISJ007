834eec515bc4b52131233c38c321002822e07156
sandeep bhingle <sandeep.bhingle@itshaala.com>
2024-01-12 11:12:42 +0530

    pattern programs

---
+++ core-java/src/controlStatements/loopingStatements/Demo27Pattern.java
@@ -0,0 +1,22 @@
+package controlStatements.loopingStatements;
+
+public class Demo27Pattern {
+    public static void main(String[] args) {
+        /*
+         * * * * *
+         * * * *
+         * * *
+         * *
+         *
+         * */
+
+        for (int row = 5; row >= 1; row--) {
+
+            for (int column = 1; column <= row; column++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+    }
+}
